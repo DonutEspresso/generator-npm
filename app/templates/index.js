eaeb83949a4884f3984f4f651d96b05a91fd7376
Peter Marton <pmarton@netflix.com>
2018-06-20 16:16:40 -0700

    feat(document): add JSDoc based make document

--- app/templates/index.js
+++ app/templates/index.js
@@ -1,3 +1,22 @@
 'use strict';
 
-module.exports = {};
+/**
+ * Say hello
+ * @public
+ * @param {String} [name=Jane] - name of the person
+ * @returns {String}
+ * @example
+ * <caption>
+ * With default name
+ * </caption>
+ * sayHello();
+ * @example
+ * sayHello('John');
+ */
+function sayHello(name = 'Jane') {
+    return `hello ${name}`;
+}
+
+module.exports = {
+    sayHello
+};
